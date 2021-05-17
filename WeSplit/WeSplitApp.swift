b5cc93f413d6b8e29aeb795ede1f90e0efb2e4fa
Kazi Abdullah Al Mamun <kazi.a.mamun@gmail.com>
2021-05-17 13:05:01 +0600

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Kazi Abdullah Al Mamun on 5/17/21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
