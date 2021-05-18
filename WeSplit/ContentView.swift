3f7b93b7981436e25831d8fc9efa9cb52fc91435
Kazi Abdullah Al Mamun <kazi.a.mamun@gmail.com>
2021-05-18 13:36:06 +0600

    Project complete

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,9 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = 0
+    @State private var tipsPercentage = 0
+    
+    private var totalPerPerson: Double {
+        
+        let checkAmount = Double(checkAmount) ?? 0.0
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipsSelection = Double(tipPercentages[tipsPercentage])
+        
+        let tipsAmount = checkAmount / 100 * tipsSelection
+        let grandTotal = checkAmount + tipsAmount
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2 ..< 100) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                
+                Section(header: Text("How much tip do you want to leave?")) {
+                    Picker("Tip Percentage", selection: $tipsPercentage) {
+                        ForEach(0 ..< tipPercentages.count) {
+                            Text("\(tipPercentages[$0])%")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section {
+                    Text("$\(totalPerPerson, specifier: "%.2f")")
+                }
+            }
+            .navigationBarTitle("WeSplit", displayMode: .inline)
+        }
     }
 }
 
